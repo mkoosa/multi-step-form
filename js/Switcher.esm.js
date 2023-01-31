a9198a7e8daf5661cdc76fb452a05fefec0d6152
Marcin Kosecki <marcin.kosecki@int.pl>
2023-01-31 12:35:36 +0100

    Create: -class Plan in step-2.js - activateBonus, saveChoiceOption in Switcher.esm.js to set values i plans

--- js/Switcher.esm.js
+++ js/Switcher.esm.js
@@ -4,14 +4,21 @@ const MONTH_ID = "monthId";
 const YEAR_ID = "yearId";
 const BOX_ID = "boxId";
 const OPTION_CLASS = ".switch__option";
-const year = "Yearly";
-const MONTH = "Monthly";
+const MONTHLY_POS = "flex-end";
+const YEARLY_POS = "flex-start";
+
+const OPTION_BONUS_CLASS = ".option_bonus";
+
+const MARINE_BLUE = "hsl(213, 96%, 18%)";
+const COOL_GREY = "hsl(231, 11%, 63%)";
 
 export default class Switch extends Common {
   constructor(elementId) {
     super(elementId);
     this.options();
     this.chooseOption();
+    this.choiceOptions = { month: false, year: false };
+    this.bonusElements = document.querySelectorAll(OPTION_BONUS_CLASS);
   }
 
   options() {
@@ -24,23 +31,40 @@ export default class Switch extends Common {
   chooseOption() {
     this.box.addEventListener("click", (e) => this.change(e));
   }
+
   change(e) {
     let switcher = e.target;
     let styles = window.getComputedStyle(switcher);
     let position = styles.justifyContent;
+    this.saveChoiceOption(position);
     switcher.style.justifyContent =
-      position === "flex-start" ? "flex-end" : "flex-start";
-    this.changeTextColor(switcher)
+      position === YEARLY_POS ? MONTHLY_POS : YEARLY_POS;
+    this.changeTextColor(switcher);
   }
 
   changeTextColor(element) {
     let position = element.style.justifyContent;
-    this.year.style.color = position === "flex-end" ? 'hsl(213, 96%, 18%)' : 'hsl(231, 11%, 63%)';
-    this.mouth.style.color = position !== "flex-end" ? 'hsl(213, 96%, 18%)' : 'hsl(231, 11%, 63%)';
-
-    
+    this.year.style.color = position === MONTHLY_POS ? MARINE_BLUE : COOL_GREY;
+    this.mouth.style.color = position !== MONTHLY_POS ? MARINE_BLUE : COOL_GREY;
   }
 
-  
+  saveChoiceOption(value) {
+    if (value === MONTHLY_POS) {
+      this.choiceOptions.month = true;
+      this.choiceOptions.year = false;
+      this.activateBonus(true);
+      return;
+    }
+    this.choiceOptions.year = true;
+    this.choiceOptions.month = false;
+    this.activateBonus(false);
+  }
 
+  activateBonus(value) {
+    if (value) {
+      this.bonusElements.forEach((element) => (element.style.display = "none"));
+      return;
+    }
+    this.bonusElements.forEach((element) => (element.style.display = "block"));
+  }
 }
--- js/step-2.js
+++ js/step-2.js
@@ -1,3 +1,43 @@
-import Switcher from './Switcher.esm.js'
-const SWITCHER_ID = 'switcherId';
-const switcher = new Switcher(SWITCHER_ID);
+import Common from "./Common.esm.js";
+import Switch from "./Switcher.esm.js";
+import Costs from "./index.js";
+
+const SWITCHER_ID = "switcherId";
+const ARCADE_COST_ID = "arcCostId";
+const ADVANCED_COST_ID = "advCostId";
+const PRO_COST_ID = "proCostId";
+
+const OPTION_BONUS_CLASS = ".option_bonus";
+
+class Plans extends Common {
+  constructor() {
+    super();
+    let _switcher = new Switch(SWITCHER_ID);
+    this.getSwitcher = () => _switcher;
+    this.bindToElements();
+    this.setCosts();
+  }
+
+  bindToElements() {
+    this.arcadeCost = this.bindToElement(ARCADE_COST_ID);
+    this.advancedCost = this.bindToElement(ADVANCED_COST_ID);
+    this.proCost = this.bindToElement(PRO_COST_ID);
+    this.switcher = this.getSwitcher();
+    this.switcher.box.addEventListener("click", () => this.setCosts());
+  }
+
+  setCosts() {
+    if (this.switcher.choiceOptions.month) {
+      this.arcadeCost.innerText = `${Costs.arcade.month}/mo`;
+      this.advancedCost.innerText = Costs.advanced.month;
+      this.proCost.innerText = Costs.pro.month;
+    } else {
+      this.arcadeCost.innerText = `${Costs.arcade.year}/yr`;
+      this.advancedCost.innerText = Costs.advanced.year;
+      this.proCost.innerText = Costs.pro.year;
+    }
+  }
+}
+
+const plans = new Plans();
+
