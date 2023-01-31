7fb4d555c4ddacadd2a820d13b8b3f963503a654
Marcin Kosecki <marcin.kosecki@int.pl>
2023-01-31 12:31:00 +0100

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const costs = {
+  arcade: {
+    month: 9,
+    year: 90,
+  },
+  advanced: {
+    month: 12,
+    year: 120,
+  },
+  pro: {
+    month: 15,
+    year: 150,
+  },
+};
+
+export default costs;
