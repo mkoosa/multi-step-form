6ee3eb6ad6b126b1d8d1c9499012bb407654f431
Marcin Kosecki <marcin.kosecki@int.pl>
2023-02-14 19:08:37 +0100

    Add method matchOptionElement to file SetOptions.esm.js

--- js/SetOptions.esm.js
+++ js/SetOptions.esm.js
@@ -1,5 +1,4 @@
 import Common from "./Common.esm.js";
-
 const OPTION_ELEMENT_CLASS = ".option";
 const OPTION_COST_ELEMENT_CLASS = ".option__cost";
 const ACTIVE_CLASS = "active";
@@ -9,7 +8,6 @@ export default class SetOptions extends Common {
     super(ELEMENT_ID);
     this.getOptionCostElements();
     this.options = document.querySelectorAll(OPTION_ELEMENT);
-    console.log(this.options);
     this.costs = document.querySelectorAll(OPTION_COST);
     this.visibility = true;
     this.setListeners();
@@ -55,11 +53,9 @@ export default class SetOptions extends Common {
   removeClass() {
     this.options.forEach((option) => option.classList.remove(ACTIVE_CLASS));
   }
+  
+  matchOptionElement(e) {
+    const element = e.target.closest('.option').getAttribute("data-name");
+    return element    
+  }
 }
-
-
-function kocica() {
-  return 'dwie kocice'
-}
-
-console.log(kocica());
