994633325151b841d6eaea283317807c4ec83a71
Marcin Kosecki <marcin.kosecki@int.pl>
2023-02-16 08:45:36 +0100

    Create methods: getPeriodTime, setPrices, createUser, save to display option prices

--- js/step-3.esm.js
+++ js/step-3.esm.js
@@ -1,30 +1,64 @@
 import Common from "./Common.esm.js";
+import Storage from "./Storage.esm.js";
+import Costs from "./Costs.esm.js";
+import User from "./User.esm.js";
+const OPTION_ELEMENT_CLASS = ".option";
+const ACTIVE_CLASS = "active";
+const OPTION_COST_CLASS = ".option__cost";
 
-const CHECKBOX_CLASS = ".css-checkbox";
-const ACTIVE_CLASS = 'active';
+const CHECKBOX_ID = "#checkboxId";
 const WRAPPER_ID = "wrapper";
+const key = "user";
 
 class ThirdStep extends Common {
   constructor(elementId) {
-      super(elementId);
-      this.bindToElements();
-      this.setListeners();
+    super(elementId);
+    this.costs = new Costs(OPTION_COST_CLASS);
+    this.bindToElements();
+    this.setListeners();
+    this.getPeriodTime();
+    this.setPrices();
   }
 
   bindToElements() {
-    this.checkBoxes = document.querySelectorAll(CHECKBOX_CLASS);
+    this.checkBoxes = document.querySelectorAll(CHECKBOX_ID);
   }
-  addClassToCheckBox() {}
-  addClass(e) {
-    e.target.classList.add(ACTIVE_CLASS)
+
+  chooseOption(e) {
+    const option = e.target.closest(OPTION_ELEMENT_CLASS);
+    option.classList.toggle(ACTIVE_CLASS);
+
+    let name = option.dataset.name;
+   
+    this.createUser(name, this.timePeriod)
   }
 
   setListeners() {
-    this.checkBoxes.forEach((checkBox) =>
-      checkBox.addEventListener("click", (e)=>this.addClass(e))
-    );
+    this.checkBoxes.forEach((checkBox) => {
+      checkBox.addEventListener("click", (e) => this.chooseOption(e));
+    });
+  }
+
+  getPeriodTime() {
+    let storage = new Storage();
+    this.timePeriod = storage.getItemFromStorage(key);
+    this.timePeriod = JSON.parse(this.timePeriod).period;
+    this.selectedOptionStepBeforeIsMonth =
+      this.timePeriod === "month" ? true : false;
+  }
+
+  setPrices() {
+    this.costs.setPrice(this.selectedOptionStepBeforeIsMonth);
+  }
+
+  createUser(name, period) {
+    this.user = new User(name, period);
+    this.save(key, JSON.stringify(this.user.options))
+  }
+  save(key, value) {
+    let storage = new Storage()
+    storage.createStorage(key, value)
   }
 }
 
 const thirdStep = new ThirdStep(WRAPPER_ID);
-// console.log("🚀 ~ file: step-3.esm.js:18 ~ thirdStep", thirdStep)
