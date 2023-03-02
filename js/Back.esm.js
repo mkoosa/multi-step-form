842bca4557462ccec6790baf4737241d0baebc14
Marcin Kosecki <marcin.kosecki@int.pl>
2023-03-02 10:17:44 +0100

    Add getValues to class BackBtn

--- js/Back.esm.js
+++ js/Back.esm.js
@@ -1,7 +1,12 @@
 import NextBtn from "./NextBtn.esm.js";
 
 export default class BackBtn extends NextBtn {
-  constructor(value, btn) {
+  constructor(value, btn, key) {
     super(value, btn);
+    this.key = key;
+  }
+
+  getValues() {
+    return JSON.parse(localStorage.getItem(this.key));
   }
 }
